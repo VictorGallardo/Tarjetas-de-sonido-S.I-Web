4bbc638146c2daec6a1da9a893a8d887652fd074
Víctor Gallardo <victor.gallardo@iescampanillas.com>
2019-12-06 14:18:48 +0100

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,3 @@
-
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -9,3 +8,25 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+const navSlide = ()=> {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', ()=> {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        burger.classList.toggle('toggle');
+    });
+
+
+
+}
+
+navSlide();
